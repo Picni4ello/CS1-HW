6b3b5de0de7ebb45b81cd45a62284249f5b587b2
Tatyana <picni4ello@mail.ru>
2020-04-03 23:28:32 +0300

    Lesson04

---
+++ Lesson04/Homework04/Homework04/MyArray.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework04
+{
+    class MyArray
+    {
+
+        int[] a;
+        
+
+        /*public MyArray(int n, int el)
+        {
+                a = new int[n];
+                for (int i = 0; i < n; i++)
+                    a[i] = el;
+        }*/
+            // Создание массива и заполнение его случайными числами от min до max
+        public MyArray(int n)
+        {
+                a = new int[n];
+                Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+                a[i] = rnd.Next(-10000, 10001);
+        }
+        public MyArray(int n, int firstEL, int step)
+        {
+            a = new int[n];
+            a[0] = firstEL;
+            for (int i = 1; i < n; i++)
+                a[i] = a[i - 1] + step;
+        }
+
+        public int Sum
+        {
+            get
+            {
+                int sum = 0;
+                for (int i = 0; i < a.Length; i++)
+                    sum += a[i];
+                return sum;
+            }
+        }
+
+        public int Max
+        {
+            get
+            {
+                int max = a[0];
+                for (int i = 1; i < a.Length; i++)
+                    if (a[i] > max) max = a[i];
+                return max;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                int min = a[0];
+                for (int i = 1; i < a.Length; i++)
+                    if (a[i] < min) min = a[i];
+                return min;
+            }
+        }
+        
+        public int CountPositiv
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < a.Length; i++)
+                    if (a[i] > 0) count++;
+                return count;
+            }
+        }
+
+        public int MaxCount
+        {
+            get
+            {
+                int max = Max;
+                int count = 0;
+                for (int i = 0; i < a.Length; i++)
+                    if (a[i] == max) count++;
+                return count;
+            }
+        }
+
+        public int Length
+        {
+            get;
+            
+        }
+        public int Inverse
+        {
+            set
+            {
+                for (int i = 0; i < a.Length; i++)
+                    a[i] = a[i] * -1;
+               
+            }
+        }
+
+
+        public int Multi
+        {
+            set
+            {
+                for (int i = 0; i < a.Length; i++)
+                    a[i] = a[i] * value;
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (int v in a)
+                s = s + v + " ";
+            return s;
+        }
+        public MyArray(string filename)
+        {
+            StreamReader sr = null;
+            try
+            {
+                sr = new StreamReader(filename);
+                int n = int.Parse(sr.ReadLine());
+                List<string> list = new List<string>();
+                for (int i = 0; i < n; i++)
+                    list.Add(sr.ReadLine());
+                a = new int[n];
+                for (int i = 0; i < n; i++)
+                    a[i] = int.Parse(list[i]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sr?.Close();
+            }
+        }
+
+
+
+        public void WriteToFile(string filename)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(filename);
+                sw.WriteLine(a.Length.ToString());
+                foreach (int element in a)
+                    sw.WriteLine(element);
+
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            finally
+            {
+                if (sw != null) sw.Close();
+                //sw?.Close();
+            }
+        }
+    }
+    
+}
---
+++ Lesson04/Homework04/Homework04/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework04
+{
+
+    //Выполнила Татьяна Гринева
+
+    class Program
+    {
+        static int GetNumberOfPairs(int[] array, int length)
+        {
+            int count = 0;
+            for (int i = 0; i < length - 1; i++)
+            {
+                if (array[i] % 3 == 0 || array[i + 1] % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static void Task1()
+        {
+            int arrayLength = 20;
+            int[] myArray = new int[arrayLength];
+            Random rnd = new Random();
+            int result;
+
+            Console.WriteLine("Данная программа считает количество пар элементов, в которых хотя бы одно число делится на 3.");
+            Console.Write("\nВ следующем массиве [ ");
+            for (int i = 0; i < arrayLength; i++)
+            {
+                myArray[i] = rnd.Next(-10000, 10000);
+                Console.Write(myArray[i] + ", ");
+            }
+            Console.WriteLine("\b\b ]\n");
+
+            result = GetNumberOfPairs(myArray, arrayLength);
+
+            Console.WriteLine($"Количество пар: {result}");
+
+            Console.ReadKey();
+
+        }
+
+        static void Task2()
+        {
+            MyArray a = new MyArray(10);
+            MyArray b = new MyArray(10, 0, 10);
+            Console.WriteLine($"Пример массива с увеличением каждого последующего элемента на определенное значение: " + b.ToString());
+            Console.WriteLine($"\nПример рандомного массива: "+a.ToString());
+            Console.WriteLine($"\nМаксимальное число в массиве: "+ a.Max);
+            Console.WriteLine($"\nМинимальное число в массиве: " + a.Min);
+            Console.WriteLine($"\nКоличество положительных чисел в массиве: " + a.CountPositiv);
+            Console.WriteLine($"\nСумма всех элементов массива: " + a.Sum);
+            Console.WriteLine($"\nКоличество максимальных элементов массива: " + a.MaxCount);
+            Console.WriteLine("\nЗапись массива в файл data.txt");
+            a.WriteToFile("data.txt");
+            a.Multi = 2;
+            Console.WriteLine($"\nУмножение всех элементов массива на 2: " + a.ToString());
+            MyArray c = new MyArray("data.txt");
+            Console.WriteLine("\nЧтение массива из файла data.txt: " + c.ToString());
+            c.Inverse = -1;
+            Console.WriteLine("\nИнверсия всех элементов массива: "+ c.ToString());
+            
+
+            Console.ReadKey();
+        }
+        static void Task3()
+        {
+            /*MyArray x = new MyArray("loginpass.txt");
+            Console.WriteLine(x.ToString());
+            int MaxCount = 3;
+            Console.WriteLine("Реализовать метод проверки логина и пароля.");
+            string login, password;
+
+            int count = 0;
+            do
+            {
+                Console.WriteLine("\nВведите логин");
+                login = Console.ReadLine();
+                Console.WriteLine("Введите пароль");
+                password = Console.ReadLine();
+                for (int i = 0; i < x.Length; i + 2)
+                {
+                    if (x[i] == login && x[i++] == password);
+                    {
+                        Console.WriteLine("Доступ разрешен");
+                        break;
+                    }
+                    count++;
+                    Console.WriteLine("Доступ запрещен. У вас осталось " + (MaxCount - count) + " попыт" + ((MaxCount - count) == 1 ? "ка" : "ки"));
+                }
+            }
+            while (count <= MaxCount);
+
+                Console.ReadKey();
+            */
+            
+        }
+
+        static int Menu()
+        {
+            Console.WriteLine("1 - Задание 1");
+            Console.WriteLine("2 - Задание 2");
+            //Console.WriteLine("3 - Задание 3");
+            int m = int.Parse(Console.ReadLine());
+            return m;
+        }
+
+        static void Main(string[] args)
+        {
+            int m = Menu();
+
+            switch (m)
+            {
+                case 1:
+                    Task1();
+                    break;
+                case 2:
+                    Task2();
+                    break;
+                /*case 3:
+                    Task2();
+                    break;*/
+
+                default:
+                    Console.WriteLine("Неверная цифра");
+                    break;
+            }
+        }
+    }
+}
