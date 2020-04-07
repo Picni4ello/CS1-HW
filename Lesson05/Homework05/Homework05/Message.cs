03a38886728085d1b1e24afb6dc78b108b95eb32
Tatyana <picni4ello@mail.ru>
2020-04-07 22:51:49 +0300

    Lesson05

---
+++ Lesson05/Homework05/Homework05/Message.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework05
+{
+    static class Message
+    {
+        static public string text;
+
+        static Message()
+        {
+            text = "Повседневная практика показывает, что новая модель проект организационной деятельности напрямую зависит от экономической целесообразности опыта принимаемых решений! С другой стороны рамки и место обучения кадров в значительной степени обуславливает создание позиций, занимаемых участниками в отношении поставленных задач. Практический опыт показывает, что выбранный нами инновационный путь представляет собой интересный эксперимент проверки форм воздействия? \nПрактический опыт показывает, что консультация с профессионалами из IT влечет за собой процесс внедрения и модернизации позиций, занимаемых участниками в отношении поставленных задач! С другой стороны повышение уровня гражданского сознания способствует подготовке и реализации существующих финансовых и административных условий? Не следует, однако, забывать о том, что начало повседневной работы по формированию позиции обеспечивает широкому кругу специалистов участие в формировании новых предложений? Таким образом, курс на социально - ориентированный национальный проект обеспечивает актуальность дальнейших направлений развития проекта?";
+        }
+
+        
+        static public void GetWordsByLength(int len)
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            //Console.WriteLine("Вывод слов, длинной не более " + len + ": " );
+            foreach (string word in words)
+            {
+                if (word == "")
+                    continue;
+                if (word.Length <= len)
+                    Console.Write(word + " ");
+            }
+        }
+                
+        static public void DeleteWordByEndChar(char ch)
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            //Console.WriteLine("Будут удалены слова, оканчивающиеся на символ '" + ch + "': ");
+            foreach (string word in words)
+            {
+                if (word == "")
+                    continue;
+                if (word[word.Length - 1] == ch)
+                {
+                    Console.Write(word + " ");
+                    text.Replace(word, "");
+                }
+            }
+            
+        }
+
+       
+        static public string FindMaxLengthWord()
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            string maxWord = words[0];
+            int max = words[0].Length;
+
+            foreach (string word in words)
+            {
+                if (word.Length > max)
+                {
+                    max = word.Length;
+                    maxWord = word;
+                }
+            }
+            return maxWord;
+        }
+
+        static public StringBuilder GetLongWordsString()
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            StringBuilder result = new StringBuilder();
+            int max = Message.FindMaxLengthWord().Length;
+            foreach (string word in words)
+            {
+                if (word.Length == max)
+                {
+                    result.Append(word.ToLower() + " ");
+                }
+            }
+            return result;
+        }
+
+        static public void FrequencyAnalysis(string[] words, string text)
+        {
+            Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+
+            string[] textWords = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            foreach (string word in words)
+            {
+                foreach (string wordInText in textWords)
+                {
+                    if (word == "")
+                        continue;
+                    if (wordInText == word)
+                    {
+                        if (word == "")
+                            continue;
+                        if (wordFrequency.ContainsKey(word))
+                            wordFrequency[word] += 1;
+                        else
+                            wordFrequency.Add(word, 1);
+                    }
+                }
+            }
+
+            ICollection<string> keys = wordFrequency.Keys;
+
+            String result = String.Format("{0,-10} {1,-10}\n\n", "Слово", "Частота появления");
+
+            foreach (string key in keys)
+                result += String.Format("{0,-10} {1,-10:N0}\n",
+                                   key, wordFrequency[key]);
+            Console.WriteLine($"\n{result}");
+        }
+    }
+}
---
+++ Lesson05/Homework05/Homework05/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Homework05
+{
+    class Program
+    {
+        // Выполнила Гринева Татьяна
+
+        //Метод проверки на соответствие логина требованиям без регулярных выражений
+        static bool CheckLogin(string login)
+        {
+            int length = login.Length;
+            if (length >= 2 && length <= 10)
+            {
+                bool check = true;
+                char letter = login[0];
+                if (Char.IsDigit(letter))
+                    return false;
+                for (int i = 1; i < length; i++)
+                {
+                    letter = login[i];
+                    if (!(Char.IsDigit(letter) || IsLatinLetter(letter)))
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                    return true;
+            }
+            return false;
+        }
+
+        //Метод проверки на соответствие логина требованиям через регулярные выражения
+     
+        static bool CheckLoginReg(string login)
+        {
+            char letter = login[0];
+            if (Char.IsDigit(letter))
+                return false;
+            if (!Regex.IsMatch(login, @"^[a-zA-Z0-9]+${2,10}"))
+                return false;
+            return true;
+        }
+
+        
+        private static bool IsLatinLetter(char letter)
+        {
+            int num = letter;
+            if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122))
+                return true;
+            else
+                return false;
+        }
+
+        static void Task1()
+        {
+
+            int MaxCount = 3;
+            Console.WriteLine("Программа проверки правильности ввода логина");
+            string login;
+            int count = 0;
+            do
+            {
+                Console.WriteLine("\nВведите логин");
+                login = Console.ReadLine();
+                if (CheckLogin(login) && CheckLoginReg(login))
+                {
+                    Console.WriteLine("Введен правильный логин");
+                    break;
+                }
+                count++;
+                Console.WriteLine("Неверный ввод. \nДолжны быть соблюдены следующие условия:"
+                        + "\nлогин должен быть не менее 2 и не более 10 символов;"
+                        + "\nможно использовать только буквы латинского алфавита или цифры;"
+                        + "\nцифра не может быть первой. \nУ вас осталось " + (MaxCount - count) + " попыт" + ((MaxCount - count) == 1 ? "ка" : "ки"));
+            }
+            while (count <= MaxCount);
+
+            Console.ReadKey();
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Программа демонстрации возможностей статического класса Message.");
+
+            
+            Console.WriteLine("\nИмеется следующий текст: \n"+Message.text);
+            Console.WriteLine("\nВыведем слова текста, которые содержат не более 5 букв:");
+            Message.GetWordsByLength(5);
+
+            Console.WriteLine();
+            Console.Write("\nУдалим из текста слова, заканчивающиеся на 'я': ");
+            Message.DeleteWordByEndChar('я');
+
+            Console.WriteLine();
+            Console.WriteLine("\nСамое длинное слово в тексте: " + Message.FindMaxLengthWord());
+
+
+            Console.WriteLine("\nСформированная строка StringBuilder из самых длинных слов сообщения: \n" + Message.GetLongWordsString());
+
+            Console.WriteLine("\nПроизведём частотный анализ текста: ");
+            string[] arr = {"модель", "новая", "степени", "опыт", "проект"};
+            Message.FrequencyAnalysis(arr, Message.text);
+
+            Console.ReadKey();
+        }
+        
+        static int Menu()
+        {
+            Console.WriteLine("1 - Программа проверки правильности логина");
+            Console.WriteLine("2 - Демонстрация работы класса Messsage");
+            int m = int.Parse(Console.ReadLine());
+            return m;
+        }
+        static void Main(string[] args)
+        {
+            int m = Menu();
+
+            switch (m)
+            {
+                case 1:
+                    Task1();
+                    break;
+                case 2:
+                    Task2();
+                    break;
+
+                default:
+                    Console.WriteLine("Неверная цифра");
+                    break;
+            }
+        }
+    }
+}
