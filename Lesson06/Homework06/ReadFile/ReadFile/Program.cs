a76577e99176c681ff1489df4e1fa7f4c2ec9d33
Tatyana <picni4ello@mail.ru>
2020-04-11 12:18:53 +0300

    Lesson06

---
+++ Lesson06/Homework06/ReadFile/ReadFile/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace ReadFile
+{
+
+    //Выполнила Гринева Татьяна
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long kbyte = 1024;
+            long mbyte = 1024 * kbyte;
+            long gbyte = 1024 * mbyte;
+            long size = mbyte;
+            //Write FileStream
+            //Write BinaryStream
+            //Write StreamReader/StreamWriter
+            //Write BufferedStream
+            Console.WriteLine("Записываем файлы при помощи разных потоков:");
+            Console.WriteLine("FileStream. Milliseconds:{0}", FileStreamSampleWrite("bigdata0.bin", size));
+            Console.WriteLine("BinaryStream. Milliseconds:{0}", BinaryStreamSampleWrite("bigdata1.bin", size));
+            Console.WriteLine("StreamWriter. Milliseconds:{0}", StreamWriterSampleWrite("bigdata2.bin", size));
+            Console.WriteLine("BufferedStream. Milliseconds:{0}", BufferedStreamSampleWrite("bigdata3.bin", size));
+
+            Console.WriteLine("Чтение файлов при помощи разных потоков. В задачу не входит вывод на экран");
+            byte[] bytesFromFileStream = FileStreamSampleRead("bigdata0.bin");
+            int[] integersFromBinatyStream = BinaryStreamSampleRead("bigdata1.bin");
+            string stringFromSreamReader = StreamReaderSample("bigdata2.bin");
+            byte[] bytesFromBufferedStream = BufferedStreamSampleRead("bigdata3.bin");
+
+            Console.ReadKey();
+        }
+
+        static long FileStreamSampleWrite(string filename, long size)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            //FileStream fs = new FileStream("bigdata.bin", FileMode.CreateNew, FileAccess.Write);
+            for (int i = 0; i < size; i++)
+                fs.WriteByte(0);
+            fs.Close();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        static byte[] FileStreamSampleRead(string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            byte[] byteArray = new byte[fs.Length];
+            for (int i = 0; i < fs.Length; i++)
+                byteArray[i] = (byte)fs.ReadByte();
+            fs.Close();
+            return byteArray;
+        }
+
+         static long BinaryStreamSampleWrite(string filename, long size)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            for (int i = 0; i < size; i++)
+                bw.Write((byte)0);
+            fs.Close();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+
+        static int[] BinaryStreamSampleRead(string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            int[] intArr = new int[fs.Length / 4];
+            BinaryReader br = new BinaryReader(fs);
+            for (int i = 0; i < fs.Length / 4; i++)
+                intArr[i] = br.ReadInt32();
+            fs.Close();
+            return intArr;
+        }
+
+        static long StreamWriterSampleWrite(string filename, long size)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            for (int i = 0; i < size; i++)
+                sw.Write(0);
+            fs.Close();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        static string StreamReaderSample(string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string result = sr.ReadToEnd();
+            fs.Close();
+            return result;
+        }
+
+        static long BufferedStreamSampleWrite(string filename, long size)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            int countPart = 4;//количество частей
+            int bufsize = (int)(size / countPart);
+            byte[] buffer = new byte[size];
+            BufferedStream bs = new BufferedStream(fs, bufsize);
+            //bs.Write(buffer, 0, (int)size);//Error!
+            for (int i = 0; i < countPart; i++)
+                bs.Write(buffer, 0, (int)bufsize);
+            fs.Close();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        static byte[] BufferedStreamSampleRead(string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            int countPart = 4;//количество частей
+            int bufsize = (int)(fs.Length / countPart);
+            byte[] buffer = new byte[fs.Length];
+            BufferedStream bs = new BufferedStream(fs, bufsize);
+            //bs.Write(buffer, 0, (int)size);//Error!
+            for (int i = 0; i < countPart; i++)
+                bs.Read(buffer, 0, (int)bufsize);
+            fs.Close();
+            return buffer;
+        }
+    }
+    
+}
